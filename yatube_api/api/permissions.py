3adc9585ff68afa8fff4f84af16de4d3a4a4d07f
Рина Петрова <dopioxdiavolo@gmail.com>
2025-03-28 00:30:00 +0300

    1

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Разрешаем только авторизованным пользователям делать POST, PUT,
+        # DELETE
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # Разрешаем GET, HEAD, OPTIONS всем
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Только автор может изменять или удалять объект
+        return request.user == obj.author
